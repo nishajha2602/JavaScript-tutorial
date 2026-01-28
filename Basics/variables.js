e306bde64fd058ebd54eaca6ffbc931c6f16fcd1
NISHA JHA <157301528+nishajha2602@users.noreply.github.com>
2026-01-28 15:59:31 +0000

    understanding variables

---
+++ Basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144554;
+let accountEmail = "nishaha2602@gmail.com";
+var accountPassword = "123456";
+accountCity =  "Jaipur";
+let accountState;
+
+// accountId = 2; not allowed const variable
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+prefer not to use var 
+because of the issue of block scope and functional scope
+*/
