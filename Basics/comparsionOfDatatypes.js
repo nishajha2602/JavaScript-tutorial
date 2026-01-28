5422af5e590236d5bbbbf1383e70dd5f54a10621
NISHA JHA <157301528+nishajha2602@users.noreply.github.com>
2026-01-28 19:15:14 +0000

    understanding memory

--- Basics/comparsionOfDatatypes.js
+++ Basics/comparsionOfDatatypes.js
@@ -2,3 +2,20 @@ console.log(undefined==0)
 console.log(undefined<=0)
 // === check data as well as datatype
 console.log("2"===2)
+
+//Reference (Non Promitive datatypes)
+
+const vehicle = ["Car","Bike","Cycle","Truck"]; //array
+let myObj = {
+    name: "Nisha",
+    age: 21,
+}//object
+
+const myFunction = function(){
+    console.log("Hello World!");
+}//Functions
+
+//********************Memory***************************/
+
+//stack(Primitive) , Heap(Non-Primitive)
+
