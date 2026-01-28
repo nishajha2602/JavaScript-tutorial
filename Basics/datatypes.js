df4db360308ae5af89fd50231a2a546513e31aa3
NISHA JHA <157301528+nishajha2602@users.noreply.github.com>
2026-01-28 16:20:44 +0000

    understanding datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,19 @@
+"uses strict" // treat all JS code as newer version
+
+//alert(2+2) we are using nodejs, not browser
+
+console.log(3
+    +3) //code readability should be high
+
+console.log("Nisha");
+
+let name = "JavaScript" //String => ""
+
+let age = 2026 //number => 2 to power 53
+
+let isLoggedIn = false //boolean =>true/false
+
+let state  //undefined
+
+//typeof is a function
+console.table(typeof([name,age,isLoggedIn,state])) //object
