aa830cf0afb637c55c0074aca1966fd27b2564c1
NISHA JHA <157301528+nishajha2602@users.noreply.github.com>
2026-02-01 13:53:40 +0000

    objects part1

---
+++ Basics/objects.js
@@ -0,0 +1,36 @@
+//singleton -- Object.create
+
+//object literals
+
+const mySym = Symbol(123);
+
+const user = {
+    name: "Nisha",
+    "full name": "Nisha Jha",
+    age: 18,
+    [mySym]: 123,  //[] to access symbols
+    location: "Delhi",
+    email: "nishajha@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(user.email);
+// console.log(user["full name"]);
+// console.log(user[mySym]);
+
+//freeze object
+//Object.freeze(user);
+user.email = "nishajha@chatgpt.com";
+console.log(user);
+
+user.greeting = function(){
+    console.log("Hello JS user");
+}
+
+user.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(user.greeting());
+console.log(user.greetingTwo());
