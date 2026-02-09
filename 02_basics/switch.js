97506a99c14c806eef036ed809ad8dfded31a804
NISHA JHA <157301528+nishajha2602@users.noreply.github.com>
2026-02-09 15:30:51 +0000

    uderstanding switch

---
+++ 02_basics/switch.js
@@ -0,0 +1,47 @@
+const month = "jul";
+
+switch (month){
+    case "jan" :{
+    console.log("january");
+    break;    
+}
+    case "feb" :{
+    console.log("february");
+    break;    
+}
+    case "mar" :{
+    console.log("march");
+    break;    
+}
+    case "apr" :{
+    console.log("april");
+    break;    
+}
+    case "may" :{
+    console.log("may");
+    break;    
+}
+    case "jun" :{
+    console.log("june");
+    break;    
+}
+    case "jul" :{
+    console.log("july");
+    break;    
+}
+    case "aug" :{
+    console.log("august");
+    break;    
+}
+    case "sep" :{
+    console.log("september");
+    break;    
+}
+    case "oct" :{
+    console.log("october");
+    break;    
+}
+default:{
+    console.log("invalid month");
+}
+}
