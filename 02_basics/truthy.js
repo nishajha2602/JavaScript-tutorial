ada89185a38f79dae091bc2cb194bbcb17f1e061
NISHA JHA <157301528+nishajha2602@users.noreply.github.com>
2026-02-09 16:08:40 +0000

    truthy and falsey values

---
+++ 02_basics/truthy.js
@@ -0,0 +1,12 @@
+const user = "Nisha";
+
+if(user){
+    console.log(`Hello ${user}`);
+}
+else{
+    console.log(`hello guest`);
+}
+
+//falsey values: false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+//truthy values: "0", 'false', [], {}, function(){},
