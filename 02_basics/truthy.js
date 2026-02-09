2dbfe00ef59e178432be68f104fe49410ccfa567
NISHA JHA <157301528+nishajha2602@users.noreply.github.com>
2026-02-09 16:43:53 +0000

    ?? operator

--- 02_basics/truthy.js
+++ 02_basics/truthy.js
@@ -10,3 +10,25 @@ else{
 //falsey values: false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
 //truthy values: "0", 'false', [], {}, function(){},
+
+
+//Nullish Coalescing Operator (??): null, undefined
+
+let val1;
+val1 = 5 ?? 10;
+console.log(val1); //output: 5
+
+let val2;
+val2 = null ?? 10;
+console.log(val2); //output 10
+
+let val3;
+val3 = undefined ?? 5;
+console.log(val3);  //output 5
+
+
+// ternary operator:  condition ? true : false
+
+let price = 100;
+
+price > 50 ? console.log("greater than 50") : console.log("less than 50");
