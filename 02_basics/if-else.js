62de2578baeea33fa07a7ad389fe7fe2c44561b8
NISHA JHA <157301528+nishajha2602@users.noreply.github.com>
2026-02-09 13:55:49 +0000

    if-else

---
+++ 02_basics/if-else.js
@@ -0,0 +1,45 @@
+// const user = "Nisha";
+// if(user === "Nisha"){
+//     console.log(`hello ${user}`);
+// }
+
+// const age = 18;
+// if(age >=18){
+//     console.log("You are an adult");
+// }
+// else{
+//     console.log("You are a minor");
+// }
+
+// const isUserLoggedIn = true;
+// const hasAdminAccess = false;
+
+// // if(isUserLoggedIn && hasAdminAccess){
+// //     console.log("Welcome, Admin!");
+// // }
+// // else{
+// //     console.log("acces denied");
+// // }
+
+// if(isUserLoggedIn || hasAdminAccess){
+//     console.log("welcome user");
+// }
+// else{
+//     console.log("access denied");
+// }
+
+//nested if else
+
+    const a = 20;
+    const b = 30;
+    const c = 10;
+
+    if(a>b && a>c){
+        console.log("a is the greatest number");
+    }
+    else if(b>c){
+        console.log("b is the greatest number");
+    }
+    else{
+        console.log("c is the greatest number");
+    }
