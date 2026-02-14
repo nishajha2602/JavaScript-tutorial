43c47c95ea7126d6556923a755567c7dcbb1f5f3
NISHA JHA <157301528+nishajha2602@users.noreply.github.com>
2026-02-14 16:39:53 +0000

    map function

---
+++ iterations/Map.js
@@ -0,0 +1,9 @@
+numbers= [1,2,3,4,5,6,7,8,9,10];
+
+const add = numbers.map((nums)=> nums+ 10)
+  console.log(add);
+
+const square = numbers.map((nums)=>{
+    return nums * nums;
+})
+console.log(square);
